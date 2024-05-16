343bb6d524ef6c2f4de8f4815992f8c36b4fdef4
mnmfjt <m.fujita@methodologic.co.jp>
2024-05-16 17:25:42 +0900

    add filter_by_name function

--- python-handlers/filter.py
+++ python-handlers/filter.py
@@ -3,3 +3,7 @@ from snowflake.snowpark.functions import col
 def filter_by_role(session, table_name, role):
   df = session.table(table_name)
   return df.filter(col("role") == role)
+
+def filter_by_name(session, table_name, name):
+  df = session.table(table_name)
+  return df.filter(col("name") == name)
